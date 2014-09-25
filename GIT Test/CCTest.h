1aa7ad6b2579ac0e1ade0f331d62135513045d48
Jadie Flores <jadiel.flores@copyleft.mx>
2014-09-25 13:14:40 -0500

    added new files

---
+++ GIT Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GIT Test
+//
+//  Created by Jadiel Flores on 25/09/14.
+//  Copyright (c) 2014 Jadiel Flores. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ GIT Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GIT Test
+//
+//  Created by Jadiel Flores on 25/09/14.
+//  Copyright (c) 2014 Jadiel Flores. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
